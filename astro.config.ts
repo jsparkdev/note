0aeca2c9f4a6a1d9ccfa6f264cf70985768ae975
Junseong Park <junseongpark.dev@gmail.com>
2024-10-01 21:15:59 +0900

    feat: update

--- astro.config.ts
+++ astro.config.ts
@@ -2,21 +2,31 @@ import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
 import starlightThemeRapide from "starlight-theme-rapide";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://note-5q8.pages.dev",
+  site: "https://note.jspark.dev",
   integrations: [
     starlight({
       plugins: [starlightThemeRapide()],
-      title: "Note",
+      title: "NoTe",
       logo: {
         src: "./src/assets/logo.svg",
         replacesTitle: true,
       },
+      customCss: [
+        "@fontsource/pretendard/400.css",
+        "@fontsource/pretendard/600.css",
+        "@fontsource/jetbrains-mono",
+        "./src/custom.css",
+      ],
       credits: true,
       titleDelimiter: " - ",
       social: {
         github: "https://github.com/jsparkdev/note",
+        mastodon: "https://mastodon.social/@jsparkdev",
+        "x.com": "https://x.com/jsparkdev",
       },
       components: {
         Head: "./src/components/starlight/Head.astro",
@@ -33,15 +43,18 @@ export default defineConfig({
           label: "JavaScript",
           autogenerate: { directory: "js" },
         },
-        {
-          label: "TypeScript",
-          autogenerate: { directory: "ts" },
-        },
-        {
-          label: "React",
-          autogenerate: { directory: "react" },
-        },
+        // {
+        //   label: "TypeScript",
+        //   autogenerate: { directory: "ts" },
+        // },
+        // {
+        //   label: "React",
+        //   autogenerate: { directory: "react" },
+        // },
       ],
+    }),
+    sitemap({
+      filter: (page) => !page.includes("/draft/"),
     }),
   ],
 });
--- src/content/docs/index.md
+++
@@ -1,8 +0,0 @@
----
-title: ''
-description: 지식을 기록하는 공간입니다.
-hero:
-  tagline: 모든 콘텐츠는 한글로 작성됩니다.
-  image:
-    file: ../../assets/houston.webp
----
